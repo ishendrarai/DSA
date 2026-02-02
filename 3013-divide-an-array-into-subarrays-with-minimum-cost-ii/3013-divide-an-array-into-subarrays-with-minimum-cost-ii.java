677d1108010250396fa73b422d8cbd6cf41a7fe2
Ishendra Rai <153349510+ishendrarai@users.noreply.github.com>
2026-02-02 21:37:34 +0530

    Time: 102 ms (100%), Space: 87.5 MB (65.22%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.java
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -1,83 +1,85 @@
-import java.util.*;
-
 class Solution {
-
-    static class Pair {
-        long val;
-        int idx;
-
-        Pair(long v, int i) {
-            val = v;
-            idx = i;
-        }
-    }
-
     public long minimumCost(int[] nums, int k, int dist) {
         int n = nums.length;
 
-        // smallest (k-1) elements
-        TreeSet<Pair> kMinimum = new TreeSet<>(
-            (a, b) -> a.val == b.val ? a.idx - b.idx : Long.compare(a.val, b.val)
-        );
+        // max heap: maintain k-1 smallest elements
+        PriorityQueue<Integer> pq_left =
+                new PriorityQueue<>((a, b) -> b - a);
 
-        // remaining elements
-        TreeSet<Pair> remaining = new TreeSet<>(
-            (a, b) -> a.val == b.val ? a.idx - b.idx : Long.compare(a.val, b.val)
-        );
+        // min heap: remaining elements
+        PriorityQueue<Integer> pq_right =
+                new PriorityQueue<>();
 
-        long sum = 0;
+        // lazy deletion map
+        Map<Integer, Integer> map = new HashMap<>();
 
-        // initial window
-        int i = 1;
-        while (i - dist < 1) {
-            Pair p = new Pair(nums[i], i);
-            kMinimum.add(p);
-            sum += nums[i];
-
-            if (kMinimum.size() > k - 1) {
-                Pair temp = kMinimum.last();
-                kMinimum.remove(temp);
-                sum -= temp.val;
-                remaining.add(temp);
-            }
-            i++;
-        }
+        long sum_left = 0;
+        int valid_left = 0;
 
         long result = Long.MAX_VALUE;
 
-        // sliding window
-        while (i < n) {
-            Pair add = new Pair(nums[i], i);
-            kMinimum.add(add);
-            sum += add.val;
+        for (int i = 1; i < n; i++) {
 
-            if (kMinimum.size() > k - 1) {
-                Pair temp = kMinimum.last();
-                kMinimum.remove(temp);
-                sum -= temp.val;
-                remaining.add(temp);
-            }
+            // remove element leaving the window
+            if (i >= dist + 2) {
+                int removed = nums[i - dist - 1];
 
-            result = Math.min(result, sum);
-
-            // remove element leaving window
-            Pair remove = new Pair(nums[i - dist], i - dist);
-
-            if (kMinimum.contains(remove)) {
-                kMinimum.remove(remove);
-                sum -= remove.val;
-
-                if (!remaining.isEmpty()) {
-                    Pair temp = remaining.first();
-                    remaining.remove(temp);
-                    kMinimum.add(temp);
-                    sum += temp.val;
+                if (!pq_left.isEmpty() && removed < pq_left.peek()) {
+                    map.merge(removed, 1, Integer::sum);
+                    sum_left -= removed;
+                    valid_left--;
+                } else if (!pq_left.isEmpty() && removed == pq_left.peek()) {
+                    pq_left.poll();
+                    sum_left -= removed;
+                    valid_left--;
+                } else if (!pq_right.isEmpty() && removed == pq_right.peek()) {
+                    pq_right.poll();
+                } else {
+                    map.merge(removed, 1, Integer::sum);
                 }
-            } else {
-                remaining.remove(remove);
             }
 
-            i++;
+            // add new element
+            if (pq_left.isEmpty() || i <= k - 1 || nums[i] <= pq_left.peek()) {
+                pq_left.offer(nums[i]);
+                sum_left += nums[i];
+                valid_left++;
+            } else {
+                pq_right.offer(nums[i]);
+            }
+
+            // rebalance heaps
+            if (i > k - 1) {
+                if (valid_left < k - 1) {
+                    int v = pq_right.poll();
+                    pq_left.offer(v);
+                    sum_left += v;
+                    valid_left++;
+                } else if (valid_left > k - 1) {
+                    int v = pq_left.poll();
+                    sum_left -= v;
+                    valid_left--;
+                    pq_right.offer(v);
+                }
+            }
+
+            // lazy cleanup
+            while (!pq_left.isEmpty()
+                    && map.getOrDefault(pq_left.peek(), 0) > 0) {
+                int v = pq_left.poll();
+                map.merge(v, -1, Integer::sum);
+            }
+
+            while (!pq_right.isEmpty()
+                    && map.getOrDefault(pq_right.peek(), 0) > 0) {
+                int v = pq_right.poll();
+                map.merge(v, -1, Integer::sum);
+            }
+
+            // update answer
+            if (i >= dist + 1) {
+                result = Math.min(result, sum_left);
+            }
         }
 
         return nums[0] + result;
