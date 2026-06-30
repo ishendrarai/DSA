fa3446093cc27dfc7a26aa5c1417f86c90ff875a
Ishendra Rai <Ishurai.020805@gmail.com>
2026-06-30 10:12:43 +0530

    Time: 1 ms (99.11%), Space: 42.4 MB (84.58%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -7,9 +7,6 @@ class Solution {
             return true;
         }
         while(n!=1){
-            if(n==1){
-                return true;
-            }
             if(n%2==0){
                 n=n/2;
             }else if(n%3==0){
