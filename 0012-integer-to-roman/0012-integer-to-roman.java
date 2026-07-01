3fc181ecb213be27c1362108d112c9099d6dd445
Ishendra Rai <Ishurai.020805@gmail.com>
2026-07-01 12:35:12 +0530

    Time: 7 ms (11.67%), Space: 46.5 MB (26.46%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,24 +1,30 @@
+import java.util.*;
+
 class Solution {
     public String intToRoman(int num) {
 
-        int[] values = {
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4, 1
-        };
+        LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
 
-        String[] romans = {
-            "M", "CM", "D", "CD",
-            "C", "XC", "L", "XL",
-            "X", "IX", "V", "IV", "I"
-        };
+        map.put(1000, "M");
+        map.put(900, "CM");
+        map.put(500, "D");
+        map.put(400, "CD");
+        map.put(100, "C");
+        map.put(90, "XC");
+        map.put(50, "L");
+        map.put(40, "XL");
+        map.put(10, "X");
+        map.put(9, "IX");
+        map.put(5, "V");
+        map.put(4, "IV");
+        map.put(1, "I");
 
         StringBuilder ans = new StringBuilder();
 
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                ans.append(romans[i]);
-                num -= values[i];
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            while (num >= entry.getKey()) {
+                ans.append(entry.getValue());
+                num -= entry.getKey();
             }
         }
 
